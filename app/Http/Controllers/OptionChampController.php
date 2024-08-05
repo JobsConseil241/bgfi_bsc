59fd6b0bcf899208e08a8fdc7830aa49a26f57b0
prime ESSE <sdsi@jobs.ga>
2024-08-05 13:10:56 +0100

    database configured

---
+++ app/Http/Controllers/OptionChampController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OptionChamp;
+use Illuminate\Http\Request;
+
+class OptionChampController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OptionChamp $optionChamp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OptionChamp $optionChamp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OptionChamp $optionChamp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OptionChamp $optionChamp)
+    {
+        //
+    }
+}
